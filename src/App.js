ade28e4bdab566a7779dd1032ada97dea3b9bc05
Shaun Wilson <shaunbrianwilson@gmail.com>
2024-08-23 15:37:13 +0100

    'Button voting complete'

--- src/App.js
+++ src/App.js
@@ -261,11 +261,11 @@ function FactList({ facts, setFacts }) {
 function Fact({ fact, setFacts }) {
   const [isUpdating, setIsUpdating] = useState(false);
 
-  async function handleVote() {
+  async function handleVote(columnName) {
     setIsUpdating(true);
     const { data: updatedFact, error } = await supabase
       .from('facts')
-      .update({ votesInteresting: fact.votesInteresting + 1 })
+      .update({ [columnName]: fact[columnName] + 1 })
       .eq('id', fact.id)
       .select();
     setIsUpdating(false);
@@ -300,14 +300,26 @@ function Fact({ fact, setFacts }) {
         {fact.category}
       </span>
       <div className="vote-buttons">
-        <button onClick={handleVote} disabled={isUpdating}>
+        <button
+          onClick={() => handleVote('votesInteresting')}
+          disabled={isUpdating}
+        >
           👍 {fact.votesInteresting}
         </button>
-        <button>🤯 {fact.votesMindblowing}</button>
-        <button>⛔️ {fact.votesFalse}</button>
+        <button
+          onClick={() => handleVote('votesMindblowing')}
+          disabled={isUpdating}
+        >
+          🤯 {fact.votesMindblowing}
+        </button>
+        <button onClick={() => handleVote('votesFalse')} disabled={isUpdating}>
+          ⛔️ {fact.votesFalse}
+        </button>
       </div>
     </li>
   );
 }
 
 export default App;
+
+// 1727373
